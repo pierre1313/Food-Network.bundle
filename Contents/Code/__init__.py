6f249a070001821b303ea059a930b2bd8c350264
Pierre <p.dellanave@gmail.com>
2010-11-23 21:45:01 -0800

    Fixed video parsing, art to jpeg

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -4,82 +4,81 @@ from PMS.Objects import *
 from PMS.Shortcuts import *
 
 WEB_ROOT = 'http://www.foodnetwork.com'
-VID_PAGE = 'http://www.foodnetwork.com/video-library/index.html'
+VID_PAGE = 'http://www.foodnetwork.com/food-network-full-episodes/videos/index.html'
+CLIPS_PAGE = 'http://www.foodnetwork.com/food-network-top-food-videos/videos/index.html'
+JSON_FEED = 'http://www.foodnetwork.com/food/feeds/channel-video/0,,FOOD_CHANNEL_%s_1_50_RA,00.json'
 
-CACHE_INTERVAL = 3600 * 6
+ART =  'art-default.jpg'
+ICON = 'icon-default.png'
+NAME = 'Food Network'
+
+CACHE_INTERVAL = 3600 
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler("/video/foodNetwork", MainMenu, 'Food Network', 'icon-default.png', 'art-default.png')
+  Plugin.AddPrefixHandler("/video/foodNetwork", MainMenu, NAME, ICON, ART)
+  Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
   Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
   MediaContainer.title1 = 'Food Network'
   MediaContainer.content = 'Items'
-  MediaContainer.art = R('art-default.png')
+  MediaContainer.art = R(ART)
+  DirectoryItem.thumb = R(ICON)
   HTTP.SetCacheTime(CACHE_INTERVAL)
 
-####################################################################################################
 def MainMenu():
-  dir = MediaContainer()
-  dhold = {}
-  page = XML.ElementFromURL(VID_PAGE, cacheTime=1200, isHTML=True)
-  for tag in page.xpath("//div[@id='vid-channels']/ul/li"):
-    title = tag.xpath("./h4")[0].text.strip().replace("Full Episodes", "").replace('"','')
-    channel_id = tag.xpath("./div")[0].get('id')
-    dhold[title] = Function(DirectoryItem(ShowBrowse, title=title, icon="icon-default.png"), channel_id=channel_id, title=title)
-    
-
-  kl = dhold.keys()[:]
-  kl.sort()
-  for i in kl:
-    dir.Append(dhold[i])
+  dir= MediaContainer(viewGroup = 'List')
+  dir.Append(Function(DirectoryItem(ShowFinder, title='Full Episodes'),url = VID_PAGE, title='Full Episodes'))
+  dir.Append(Function(DirectoryItem(ShowFinder, title='Videos'),url = CLIPS_PAGE, title='Videos'))
+  return dir
   
+def ShowFinder(sender,url,title):
+  dir = MediaContainer(viewGroup = 'List', title2 = title)
+  page = XML.ElementFromURL(url, cacheTime=1200, isHTML=True)
+  for tag in page.xpath("//ul[@class='playlists']/li"):
+    title = tag.xpath("./a")[0].text.replace(' Full Episodes','')
+    channel_id = tag.get("data-channel")
+    dir.Append(Function(DirectoryItem(ShowBrowse, title=title), channel_id=channel_id, title=title))  
   return dir  
 
-####################################################################################################
-def ShowBrowse(sender, channel_id, title = None):
-    page = XML.ElementFromURL(VID_PAGE, cacheTime=1200, isHTML=True)
+def GetThumb(path):
+  return DataObject(HTTP.Request(path),'image/jpeg')
 
-    dir = MediaContainer(title2=title)
-    for tag in page.xpath("//div[@id='"+channel_id+"']//a[@class='frame']"):
-		thumb = tag.xpath("./img")[0].get('src')
-		root = tag.xpath("./..//a")[1]
-		title = root.text.strip()
-		url = root.get('href')
-		try:
-			duration = root.xpath("../span")[0].text
-		except:
-			duration = None
-		if duration:
-			duration = GetDurationFromDesc(duration)			
-		dir.Append(WebVideoItem(WEB_ROOT+url, title, thumb=thumb, duration=duration))
-	
+def ShowBrowse(sender, channel_id, title = None):
+    page = XML.ElementFromURL(JSON_FEED % channel_id, cacheTime=1200, isHTML=True)
+
+    dir = MediaContainer(viewGroup = 'Details', title2=title)
+    jsonPage = HTTP.Request(JSON_FEED % channel_id)
+    jsonData = JSON.ObjectFromString(jsonPage.replace('var snapTravelingLib = ',''))
     
+    for vid in jsonData[0]['videos']:
+		thumbpath = vid['thumbnailURL']
+		title = vid['label']
+		summary = vid['description']
+		url = vid['videoURL']
+		duration = GetDurationFromString(vid['length'])	
+		dir.Append(WindowsMediaVideoItem(url, title=title,summary=summary, thumb=Function(GetThumb, path = thumbpath), duration=duration))
+	
     return dir
  
-####################################################################################################
-def GetDurationFromDesc(desc):
-  duration = ""
+def GetDurationFromString(duration):
 
   try:
-    descArray =  desc.split("(")
-    descArrayLen =  len (descArray)
-    if descArrayLen<2:
-      return ""
+    durationArray = duration.split(":")
 
-    time = descArray[descArrayLen - 1]
-    timeArray = time.split(":")
-
-    timeArrayLen = len(timeArray)
-
-    if timeArrayLen<2:
-      return ""
-
-    minutes = int(timeArray[0])
-    seconds = int(timeArray[1].split(")")[0])
-    duration = str(((minutes*60) + seconds)*1000)
+    if len(durationArray) == 3:
+      hours = int(durationArray[0])
+      minutes = int(durationArray[1])
+      seconds = int(durationArray[2])
+    elif len(durationArray) == 2:
+      hours = 0
+      minutes = int(durationArray[0])
+      seconds = int(durationArray[1])
+    elif len(durationArray)  ==  1:
+      hours = 0
+      minutes = 0
+      seconds = int(durationArray[0])
+      
+    return int(((hours)*3600 + (minutes*60) + seconds)*1000)
     
   except:
-    # There was a problem getting the duration (maybe it isn't on the description any more?) so quit with a null
-    return ""
-
-  return duration
+    return 0
