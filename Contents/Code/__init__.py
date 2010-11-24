421f2f349ebd2018ca38f8f45d8e456121603ae0
Pierre <p.dellanave@gmail.com>
2010-11-23 21:56:31 -0800

    Update to v2

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,7 +1,6 @@
 import re, urllib
-from PMS import *
-from PMS.Objects import *
-from PMS.Shortcuts import *
+
+PLUGIN_PREFIX = "/video/foodNetwork"
 
 WEB_ROOT = 'http://www.foodnetwork.com'
 VID_PAGE = 'http://www.foodnetwork.com/food-network-full-episodes/videos/index.html'
@@ -16,14 +15,14 @@ CACHE_INTERVAL = 3600
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler("/video/foodNetwork", MainMenu, NAME, ICON, ART)
+  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, NAME, ICON, ART)
   Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
   Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
   MediaContainer.title1 = 'Food Network'
   MediaContainer.content = 'Items'
   MediaContainer.art = R(ART)
   DirectoryItem.thumb = R(ICON)
-  HTTP.SetCacheTime(CACHE_INTERVAL)
+  HTTP.CacheTime = CACHE_INTERVAL
 
 def MainMenu():
   dir= MediaContainer(viewGroup = 'List')
@@ -33,7 +32,7 @@ def MainMenu():
   
 def ShowFinder(sender,url,title):
   dir = MediaContainer(viewGroup = 'List', title2 = title)
-  page = XML.ElementFromURL(url, cacheTime=1200, isHTML=True)
+  page = HTML.ElementFromURL(url, cacheTime=1200)
   for tag in page.xpath("//ul[@class='playlists']/li"):
     title = tag.xpath("./a")[0].text.replace(' Full Episodes','')
     channel_id = tag.get("data-channel")
@@ -44,10 +43,10 @@ def GetThumb(path):
   return DataObject(HTTP.Request(path),'image/jpeg')
 
 def ShowBrowse(sender, channel_id, title = None):
-    page = XML.ElementFromURL(JSON_FEED % channel_id, cacheTime=1200, isHTML=True)
+    page = HTML.ElementFromURL(JSON_FEED % channel_id, cacheTime=1200)
 
     dir = MediaContainer(viewGroup = 'Details', title2=title)
-    jsonPage = HTTP.Request(JSON_FEED % channel_id)
+    jsonPage = HTTP.Request(JSON_FEED % channel_id).content
     jsonData = JSON.ObjectFromString(jsonPage.replace('var snapTravelingLib = ',''))
     
     for vid in jsonData[0]['videos']:
